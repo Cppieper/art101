17d465733390ddc8b98d4ff7166c5cc4ebea450f
Cppieper <cpieper@ucsc.edu>
2022-10-12 19:29:08 -0700

    sushi web

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author CaitlinPieper.
+// @since  x.x.x
+//Add image/wave.jpg
+//Hover mouse over image
+//Rotate image clockwise 90 degrees
+//Move mouse off image
+//Rotate image counterclockwise 90 degrees
