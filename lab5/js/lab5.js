ee8605606513d86dfcf3d959ce3c1b2ad3f9439d
Cppieper <cpieper@ucsc.edu>
2022-10-17 17:49:57 -0700

    java site lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author:    Patyy and Arantza
+ * Created:   10.18.2022
+ * (c) Copyright by Blub Corp.
+ */
+
+ // Define Variables
+make = "Ford";
+model = "CRV";
+color = "black";
+year = 2000;
+
+ // calculate
+age = 2022 - year;
+
+ //output
+ document.writeln("make: " + make + "<br>");
+ document.writeln("model: " + model + "<br>");
+ document.writeln("color: " + color + "<br>");
+ document.writeln("year: " + year + "<br>");
+ document.writeln("age: " + age + " years<br>");
